3dc0d6239a5288e90dded0811b51be35173f03db
arock <arock1998@naver.com>
2023-02-14 11:59:11 +0900

    feat: KeyExpansion 구현

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
 
 int main() {
 
-    byte in[16] = {0x19,0x3d,0xe3,0xbe,0xa0,0xf4,0xe2,0x2b,0x9a,0xc6,0x8d,0x2a,0xe9,0xf8,0x48,0x08};
+    byte key[16] = {0x2b,0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    byte in[16] = {0x19, 0x3d, 0xe3, 0xbe, 0xa0, 0xf4, 0xe2, 0x2b, 0x9a, 0xc6, 0x8d, 0x2a, 0xe9, 0xf8, 0x48, 0x08};
     byte out[16];
+
+    KeyExpansion(key);
+    PrintW();
 
     PrintArray("Before Sbox",in);
     SubBytes(in);
@@ -16,6 +20,7 @@ int main() {
     PrintArray("After ShiftRows",in);
     MixColumns(in);
     PrintArray("After MixColumns",in);
+    
 
 }
 
@@ -54,6 +59,26 @@ void Cipher(byte* in[4*Nb], byte* out[4*Nb]) {
 }
 
 
+
+/**
+ * key expansion 
+*/
+void KeyExpansion(byte* key) {
+    for(int i = 0; i < Nk; i++) {
+        w[i] = (key[Nk*i] << 24) ^ (key[Nk*i+1] << 16) ^ (key[Nk*i+2] << 8) ^ (key[Nk*i+3]);
+    }
+    for(int i = 4; i < Nb*(Nr+1); i++) {
+        byte_4 temp = w[i-1];
+        if(i % Nk == 0) {
+            temp = SubWord(RotWord(temp)) ^ Rcon_Nr10[i/Nk -1];
+        } else if (Nk > 6 && (i % Nk == 4)) {
+            temp = SubWord(temp);
+        }
+        w[i] = w[i-Nk] ^ temp;
+    }
+}
+
+
 /**
  * sub bytes
 */
@@ -75,7 +100,6 @@ void ShiftRows(byte* state) {
         for(int j = 0; j < Nb; j++) { //1,5,9,13 //2,6,10,14 //3,6,11,15
             temp[j] = state[j*Nb+i];
         }
-
         // i번 만큼 왼쪽으로 이동
         // TODO : 더 좋은 방법이 있지 않을까?
         for(int k = i; k > 0; k--) {
@@ -86,7 +110,6 @@ void ShiftRows(byte* state) {
             temp[3] = t0;
 
         }
-
         // state에 shift 된 것들 저장
         for(int j = 0; j < Nb; j++) { //1,5,9,13 //2,6,10,14 //3,6,11,15
             state[j*Nb+i] = temp[j];
@@ -103,13 +126,50 @@ void MixColumns(byte* state) {
     for(int i = 0; i < 4*Nb; i++) {
         temp[i] = state[i];
     }
-
     for(int i = 0; i < Nb; i++) {
         state[4*i+0] = xtime(temp[4*i+0]) ^ xtime(temp[4*i+1]) ^ temp[4*i+1] ^ temp[4*i+2] ^ temp[4*i+3];
         state[4*i+1] = xtime(temp[4*i+1]) ^ xtime(temp[4*i+2]) ^ temp[4*i+2] ^ temp[4*i+3] ^ temp[4*i+0];
         state[4*i+2] = xtime(temp[4*i+2]) ^ xtime(temp[4*i+3]) ^ temp[4*i+3] ^ temp[4*i+0] ^ temp[4*i+1];
         state[4*i+3] = xtime(temp[4*i+3]) ^ xtime(temp[4*i+0]) ^ temp[4*i+0] ^ temp[4*i+1] ^ temp[4*i+2];
     }
+}
+
+
+/**
+ * add round key
+*/
+void AddRoundKey(byte* state, int round) {
+    for(int i = 0; i < Nb; i++) {
+        // state[] = state[] ^ w[round * Nb +i];
+        state[4*i+0] = state[4*i+0];
+        state[4*i+1] = state[4*i+1];
+        state[4*i+2] = state[4*i+2];
+        state[4*i+3] = state[4*i+3];
+        
+    }
+}
+
+
+/**
+ * sub word
+*/
+byte_4 SubWord(byte_4 num) {
+    byte list[4] = {0, };
+    list[0] = s_box[(num >> 24) & 0xFF];
+    list[1] = s_box[(num >> 16) & 0xFF];
+    list[2] = s_box[(num >> 8) & 0xFF];
+    list[3] = s_box[(num >> 0) & 0xFF];
+    num = (list[0] << 24) ^ (list[1] << 16) ^ (list[2] << 8) ^ (list[3]);
+    return num;
+}
+
+
+/**
+ * rot word
+*/
+byte_4 RotWord(byte_4 num) {
+    num = ((num << 8) & 0xffffff00) ^ ((num >> 24) & 0xff);
+    return num; 
 }
 
 
@@ -135,4 +195,14 @@ void PrintArray(char* str, byte* state) {
         printf("%02x, ", state[i]);
     }
     printf("\n");
+}
+
+
+/**
+ * w 출력 함수
+*/
+void PrintW() {
+    for(int i = 0; i < Nk*(Nr+1); i++) {
+        printf("w[%d] : %x \n", i, w[i]);
+    } 
 }
--- main.h
+++ main.h
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
 typedef unsigned char byte;
+typedef unsigned int byte_4;
 
+//          128     192     256            
+#define Nk  4   //  6       8
+#define Nb  4   //  4       4 
+#define Nr  10  //  12      14
 
-#define Nb  4 
-#define Nr  10
+byte_4 w[Nr+1] = {0,};
 
+byte_4 Rcon_Nr10[10] = {0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 0x20000000, 0x40000000, 0x80000000, 0x1b000000, 0x36000000};
 
 void Cipher(byte* in[4*Nb], byte* out[4*Nb]);
+
+void KeyExpansion(byte* key);
 
 void SubBytes(byte* state);                     // 테스트 완료
 
 void ShiftRows(byte* state);                    // 테스트 완료
 
-void MixColumns(byte* state);
+void MixColumns(byte* state);                   // 테스트 완료
 
 byte xtime(byte byte1);
 
+byte_4 SubWord(byte_4 num);
+
+byte_4 RotWord(byte_4 num);
+
 void PrintArray(char* str, byte* state);
+
+void PrintW();
